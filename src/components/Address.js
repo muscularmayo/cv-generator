afc877cb71a5542f113362ffc11c2b3f8f753b36
Chris Risden <christopherrisden@gmail.com>
2022-03-28 16:02:22 -0700

    changed 3 more components from class to functional

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 // export default class Address extends Component {
 //   constructor(props) {
--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,26 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Delete extends Component {
-  constructor(props) {
-    super(props)
+// export default class Delete extends Component {
+//   constructor(props) {
+//     super(props)
 
-    this.handleClick = this.handleClick.bind(this)
-  }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
 
-  handleClick(event) {
-    //this.props.index = index
-    if(this.props.education) {
-      this.props.deleteEducation(this.props.index)
-    } else if (this.props.experience) {
-      this.props.deleteExperience(this.props.index)
+//   handleClick(event) {
+//     //this.props.index = index
+//     if(this.props.education) {
+//       this.props.deleteEducation(this.props.index)
+//     } else if (this.props.experience) {
+//       this.props.deleteExperience(this.props.index)
+//     }
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>Delete</button>
+//     );
+//   }
+
+// }
+
+export default function Delete (props) {
+  const handleClick = (event) => {
+    if(props.education) {
+      props.deleteEducation(props.index)
+    } else if (props.experience) {
+      props.deleteExperience(props.index)
     }
     event.preventDefault();
   }
 
-  render() {
-    return (
-      <button onClick={this.handleClick}>Delete</button>
-    );
-  }
-
+  return (
+    <button onClick={handleClick}>Delete</button>
+  )
 }
