b5d74582eb89800b6477d5954a9ed9c5b37491ce
Chris Risden <christopherrisden@gmail.com>
2022-03-28 17:33:50 -0700

    need to do display but it's broken! did some more components

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,22 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Email extends Component {
-  constructor(props) {
-    super(props)
+// export default class Email extends Component {
+//   constructor(props) {
+//     super(props)
 
-    this.handleChange = this.handleChange.bind(this)
+//     this.handleChange = this.handleChange.bind(this)
+//   }
+
+//   handleChange (event) {
+//     this.props.changeAppState(this.props.info, event.target.value)
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.props.onSubmit}>
+//           <input placeholder="Email address" type="text" value={this.props.value}  onChange={this.handleChange}/>
+//       </form>
+//     );
+//   }
+
+// }
+
+export default function Email (props) {
+  const handleChange = (event) => {
+    props.changeAppState(props.info, event.target.value)
   }
 
-  handleChange (event) {
-    this.props.changeAppState(this.props.info, event.target.value)
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.props.onSubmit}>
-          <input placeholder="Email address" type="text" value={this.props.value}  onChange={this.handleChange}/>
-      </form>
-    );
-  }
-
+  return (
+    <form onSubmit={props.onSubmit}>
+      <input placeholder="Email Address" type="text" value={props.value} onChange={handleChange}/>
+    </form>
+  )
 }
